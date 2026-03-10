de49177252eb3e4be73bee331764d8e060ef139b
manpas-thebreathinghuman <rajafadhlurrohman@gmail.com>
2026-03-10 21:00:29 +0700

    kinda working withour excersice

--- src/test/java/id/ac/ui/cs/advprog/niuproject/service/OrderServiceImplTest.java
+++ src/test/java/id/ac/ui/cs/advprog/niuproject/service/OrderServiceImplTest.java
@@ -2,18 +2,18 @@ package id.ac.ui.cs.advprog.niuproject.service;
 import id.ac.ui.cs.advprog.niuproject.enums.OrderStatus;
 import id.ac.ui.cs.advprog.niuproject.model.Order;
 import id.ac.ui.cs.advprog.niuproject.model.Product;
+import id.ac.ui.cs.advprog.niuproject.repository.OrderRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.NoSuchElementException;
-
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
+import org.mockito.InjectMocks;
 
 @ExtendWith(MockitoExtension.class)
 class OrderServiceTest {
@@ -117,7 +117,7 @@ class OrderServiceTest {
         Order order = orders.get(1);
         doReturn(orders).when(orderRepository).findAllByAuthor(order.getAuthor());
 
-        List<Order> results = orderService.findALlByAuthor(order.getAuthor());
+        List<Order> results = orderService.findAllByAuthor(order.getAuthor());
         for (Order result : results) {
             assertEquals(order.getAuthor(), result.getAuthor());
         }
