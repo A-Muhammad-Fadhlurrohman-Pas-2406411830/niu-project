4c7c695f566d02e1003f5c18cdd0535bcdc6b4b7
manpas-thebreathinghuman <rajafadhlurrohman@gmail.com>
2026-07-05 11:57:44 +0700

    reflection 2

--- src/test/java/id/ac/ui/cs/advprog/niuproject/functional/HomePageFunctionalTest.java
+++ src/test/java/id/ac/ui/cs/advprog/niuproject/functional/HomePageFunctionalTest.java
@@ -4,4 +4,4 @@ import io.github.bonigarcia.seljup.SeleniumJupiter;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.openqa.selenium.By
+import org.openqa.selenium.By;
